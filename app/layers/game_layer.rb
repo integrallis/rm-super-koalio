a70410583465a607e07ea29d2c16dd62074246df
Brian Sam-Bodden <bsbodden@integrallis.com>
2013-09-12 10:54:48 -0700

    Step 3: Create background layer (refactor layer initialization into methods)

--- app/layers/game_layer.rb
+++ app/layers/game_layer.rb
@@ -4,12 +4,20 @@ class GameLayer < Joybox::Core::Layer
   scene
 
   def on_enter
+    load_background
+    load_tile_map
+  end
+  
+  private 
+  
+  def load_tile_map
     @tile_map = TileMap.new file_name: 'tilemap.tmx'
     self << @tile_map
   end
-
-  def on_exit
-    # Tear down
+  
+  def load_background
+    @blue_sky = LayerColor.new color: "#6365fc".to_color
+    self << @blue_sky
   end
 
 end
